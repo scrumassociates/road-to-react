46ffd9125ab58a9c7376c93c3a241f35588026ca
Dan Moran <scrumassociates@gmail.com>
2019-07-15 12:34:47 -0400

    Interactions with Forms and Events

--- src/App.js
+++ src/App.js
@@ -20,13 +20,18 @@ const list = [
   },
 ]
 
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase())
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       list,
+      searchTerm: '',
     }
     this.onDismiss = this.onDismiss.bind(this)
+    this.onSearchChange = this.onSearchChange.bind(this)
   }
 
   onDismiss(id) {
@@ -35,30 +40,42 @@ class App extends Component {
     this.setState({ list: updatedList })
   }
 
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value })
+  }
+
   render() {
     return (
       <div className="App">
-        {this.state.list.map(item => {
-          const onHandleDismiss = () => this.onDismiss(item.objectID)
-          return (
-            < div key={item.objectID} >
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <button
-                  onClick={onHandleDismiss}
-                  type="button"
-                >
-                  Dismiss
+        <form>
+          <input
+            type="text"
+            onChange={this.onSearchChange}
+          />
+        </form>
+        {this.state.list
+          .filter(isSearched(this.state.searchTerm))
+          .map(item => {
+            const onHandleDismiss = () => this.onDismiss(item.objectID)
+            return (
+              < div key={item.objectID} >
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+                <span>
+                  <button
+                    onClick={onHandleDismiss}
+                    type="button"
+                  >
+                    Dismiss
               </button>
-              </span>
-            </div>
-          )
-        })}
+                </span>
+              </div>
+            )
+          })}
       </div>
     )
   }
