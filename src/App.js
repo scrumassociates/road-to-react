3168f009933519ed6f9d0e571728bb023fb688d4
Dan Moran <dan@pop-os.localdomain>
2019-07-15 12:22:05 -0400

    Event Handler

--- src/App.js
+++ src/App.js
@@ -38,24 +38,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.list.map(item =>
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type="button"
-              >
-                Dismiss
+        {this.state.list.map(item => {
+          const onHandleDismiss = () => this.onDismiss(item.objectID)
+          return (
+            < div key={item.objectID} >
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={onHandleDismiss}
+                  type="button"
+                >
+                  Dismiss
               </button>
-            </span>
-          </div>
-        )}
+              </span>
+            </div>
+          )
+        })}
       </div>
     )
   }
