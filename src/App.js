deb42821b909aa75eb589915fb0bfecc08f9dd3a
Dan Moran <scrumassociates@gmail.com>
2019-07-15 16:23:38 -0400

    Axios instead of Fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import './App.css'
 
 const DEFAULT_QUERY = 'redux'
@@ -100,10 +101,15 @@ class App extends Component {
 
   fetchSearchTopStories(searchTerm, page = 0) {
     const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
+    axios(url)
+      .then(result => this.setSearchTopStories(result.data))
+      .catch(error => this.setState({ error }))
+    /*
     fetch(url)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => this.setState({ error }))
+    */
   }
 
   render() {
