07693aff62a2f6b9b26e59cef82a688c3b988b17
Dan Moran <scrumassociates@gmail.com>
2019-07-17 13:54:17 -0400

    Revisited: setState()

--- src/App.js
+++ src/App.js
@@ -33,6 +33,22 @@ const smallColumn = {
   width: "10%"
 }
 
+const updateSearchTopStoriesState = (hits, page) => prevState => {
+  const { searchKey, results } = prevState
+
+  const oldHits = results && results[searchKey] ? results[searchKey].hits : []
+
+  const updatedHits = [...oldHits, ...hits]
+
+  return {
+    results: {
+      ...results,
+      [searchKey]: { hits: updatedHits, page }
+    },
+    isLoading: false
+  }
+}
+
 /*
 function withFeature(Component) {
   return function (props) {
@@ -68,19 +84,7 @@ class App extends Component {
 
   setSearchTopStories(result) {
     const { hits, page } = result
-    const { searchKey, results } = this.state
-
-    const oldHits = results && results[searchKey] ? results[searchKey].hits : []
-
-    const updatedHits = [...oldHits, ...hits]
-
-    this.setState({
-      results: {
-        ...results,
-        [searchKey]: { hits: updatedHits, page }
-      },
-      isLoading: false
-    })
+    this.setState(updateSearchTopStoriesState(hits, page))
   }
 
   componentDidMount() {
